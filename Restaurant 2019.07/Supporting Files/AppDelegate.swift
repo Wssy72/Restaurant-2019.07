60ef031d6701b97363e69b83b4d1054cace0e51c
Denis Bystruev <github@learnSwift.ru>
2019-07-05 22:39:53 +0300

    Cleared App Delegate

---
+++ Restaurant 2019.07/Supporting Files/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  Restaurant 2019.07
+//
+//  Created by Denis Bystruev on 05/07/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
